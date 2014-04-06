437ed13a00d65f88bb0a70fc596d1ace848e00cf
Miguel Palhas <mpalhas@gmail.com>
2014-04-06 15:44:15 +0100

    Several cleanups and tweaks

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -6,12 +6,14 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 require 'rspec/rails'
-require 'webmock/rspec'
+require 'rspec/autorun'
+require 'capybara/webkit/matchers'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
 
 module Features
   # Extend this module in spec/support/features/*.rb
+  include Capybara::Webkit::RspecMatchers, type: :feature
 end
 
 RSpec.configure do |config|
@@ -23,7 +25,18 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
   config.use_transactional_fixtures = false
+
+  config.before(:each) do
+    Capybara.current_driver = select_driver
+  end
 end
 
-Capybara.javascript_driver = :webkit
-WebMock.disable_net_connect!(allow_localhost: true)
+def select_driver
+  if example.metadata[:js]
+    if example.metadata[:js] == :selenium
+      :selenium
+    else
+      :webkit
+    end
+  end
+end
